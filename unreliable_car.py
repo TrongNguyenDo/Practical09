493b3de3a3cb9d2b60828a8d4d145693f91ead5e
Trong Nguyen <trongnguyen.do@my.jcu.edu.au>
2022-11-15 15:50:40 +1000

    Finished unreliable_car.py

---
+++ unreliable_car.py
@@ -0,0 +1,24 @@
+from car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+
+    def __init__(self, name, fuel, reliability):
+        super.__init__(name, fuel)
+        self.reliability = reliability
+
+    def __str__(self):
+        """Return a string like a Car but with reliability."""
+        return f"{super().__str__()}, {self.reliability}% reliability"
+
+    def drive(self, distance):
+        """Drive like parent Car but depending on reliability"""
+
+        # random number from 0 and 100
+        random_number = randint(0, 100)
+        if random_number > self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
+
